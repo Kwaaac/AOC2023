16b543dd9385d6835358a32a84c48522e960e740
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2023-12-11 23:57:30 +0100

    optimisation removing 2 indexes

--- Day11/src/main/java/fr/aoc/Main.java
+++ Day11/src/main/java/fr/aoc/Main.java
@@ -65,8 +65,8 @@ public class Main {
         var res = LongStream.range(0, galaxies.size()).flatMap(_ -> {
             var p = galaxies.removeFirst();
             return galaxies.stream().mapToLong(p2 -> {
-                var emptyRowsBetween = IntStream.rangeClosed(Math.min(p.x, p2.x), Math.max(p.x, p2.x)).filter(emptyRows::contains).count();
-                var emptyColsBetween = IntStream.rangeClosed(Math.min(p.y, p2.y), Math.max(p.y, p2.y)).filter(emptyColumn::contains).count();
+                var emptyRowsBetween = IntStream.range(Math.min(p.x, p2.x + 1), Math.max(p.x, p2.x)).filter(emptyRows::contains).count();
+                var emptyColsBetween = IntStream.range(Math.min(p.y, p2.y + 1), Math.max(p.y, p2.y)).filter(emptyColumn::contains).count();
 
                 return (Math.abs(p.x - p2.x) + Math.abs(p.y - p2.y) + (expansion - 1) * emptyRowsBetween + (expansion - 1) * emptyColsBetween);
             });
