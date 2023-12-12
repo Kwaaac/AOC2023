0b4d940e16024862cd788c75fc1032b51bca962f
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2023-12-12 22:25:23 +0100

    part 2

--- Day12/src/main/java/fr/aoc/Main.java
+++ Day12/src/main/java/fr/aoc/Main.java
@@ -4,21 +4,94 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
+    private static final HashMap<Part, Long> memoization = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         var example = Paths.get("Day12", "src", "main", "resources", "example.txt");
         var input = Paths.get("Day12", "src", "main", "resources", "input.txt");
-        var start = System.currentTimeMillis();
-        puzzle1(input);
-        System.out.println(System.currentTimeMillis() - start);
+        puzzle2(input);
+
+    }
+
+    private static long countRec(String line, List<Integer> nbr) {
+        var key = new Part(line, nbr);
+        var total = memoization.get(key);
+        if (total != null) {
+            return total;
+        }
+
+        // recursion ends
+        if (line.isEmpty()) {
+            if (nbr.isEmpty()) return 1;// bon cas
+            return 0; // mauvais cas
+        }
+
+        var c = line.charAt(0);
+
+        var result = switch (c) {
+            case '.' -> countRec(stripLeftPoint(line), nbr);
+            case '?' -> countRec(STR."#\{line.substring(1)}", nbr)
+                    + countRec(STR.".\{line.substring(1)}", nbr);
+            case '#' -> {
+                // cas ou on arrête:
+                // on atteint la fin ou on ne peut plus finir le groupe
+                if (nbr.isEmpty() || line.length() < nbr.getFirst()) yield 0;
+                var currentGroup = nbr.getFirst();
+                // si on ne peut pas créer le groupe car y'a un .
+                if (line.substring(0, currentGroup).contains(".")) yield 0;
+                // il y a un # en trop à la fin, on ne peut pas finir
+                if (line.substring(currentGroup).startsWith("#")) yield 0;
+
+                var newGroups = new ArrayList<>(nbr);
+                newGroups.removeFirst();
+
+                // Si le dernier est un ?, on passe au groupe suivant
+                if (line.length() > currentGroup && line.charAt(currentGroup) == '?') {
+                    yield countRec(stripLeftPoint(line.substring(currentGroup + 1)), newGroups);
+                }
+
+                // groupe suivant
+                yield countRec(stripLeftPoint(line.substring(currentGroup)), newGroups);
+            }
+            default -> throw new IllegalStateException(STR."Unexpected value: \{c}");
+        };
+
+        memoization.put(key, result);
+        if (result < 0) System.out.println(key);
+        return result;
+    }
+
+    private static String stripLeftPoint(String line) {
+        int i = 0;
+        for (; i < line.length(); i++) {
+            if (line.charAt(i) != '.') {
+                break;
+            }
+        }
+        return line.substring(i);
+    }
+
+    private static void puzzle2(Path path) throws IOException {
+        var lines = Files.readAllLines(path);
+
+
+        var parts = lines.stream().map(line -> {
+            var tokens = line.split("\\s");
+            var input = STR."\{tokens[0]}?".repeat(4) + tokens[0];
+            var g = Arrays.stream(tokens[1].split(",")).mapToInt(Integer::parseInt).toArray();
+            var groups = IntStream.range(0, 5).flatMap(i -> Arrays.stream(g)).boxed().toList();
+            return new Part(input, groups);
+        }).toList();
+
+        var res = parts.stream().mapToLong(part -> countRec(part.line, part.nbr)).sum();
+        System.out.println(res);
+        System.out.println(STR."memoization.size() = \{memoization.size()}");
     }
 
     private static void puzzle1(Path path) throws IOException {
@@ -67,6 +140,13 @@ public class Main {
             }).sum();
 
             System.out.println(STR."res = \{res}");
+        }
+    }
+
+    private record Part(String line, List<Integer> nbr) {
+        @Override
+        public String toString() {
+            return STR."\{line} \{nbr}";
         }
     }
 }
